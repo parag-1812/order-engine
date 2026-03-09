1de214c1e9854f4edbd89035b2bb1a02b0d5436c
parag-1812 <paraggupta9988@gmail.com>
2026-03-09 22:46:20 +0530

    Admin Api's and dashboard changes

--- src/main/java/com/cloudkitchen/order_engine/config/SecurityConfig.java
+++ src/main/java/com/cloudkitchen/order_engine/config/SecurityConfig.java
@@ -49,19 +49,5 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 }
